e0a16edf19ce58ad8a717b0d251d006cbb016fa9
Christiansen MEREL <christiansen.merel@gmail.com>
2019-04-30 01:39:31 +0200

    chore: met à jour la commande de deploiement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,8 +3,4 @@
 set -x
 set -e
 
-env
-pwd
-find .
-
-sshpass -p $FTP_PASSWORD scp -v -r '_site/' blackcogns@ftp.cluster023.hosting.ovh.net:/www/test/
+sshpass -p $FTP_PASSWORD scp -v -r '_site' blackcogns@ftp.cluster023.hosting.ovh.net:/www/test
