682b95bd11036e44bd50054182abfe17217703ff
Christiansen MEREL <christiansen.merel@gmail.com>
2019-05-01 13:53:58 +0200

    chore: met à jour la commande de deploiement

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,5 +5,5 @@ set -x
 
 # sshpass -p $FTP_PASSWORD scp -v -r '_site' blackcogns@ftp.cluster023.hosting.ovh.net:/www/test
 cd _site
-ncftpput -u blackcogns -p $FTP_PASSWORD -R -v -d ftp.log -m -t 30 ftp.cluster023.hosting.ovh.net /www/test *
+ncftpput -u blackcogns -p $FTP_PASSWORD -R -v -d ftp.log -m -t 30 -E ftp.cluster023.hosting.ovh.net /www/test *
 cat ftp.log
